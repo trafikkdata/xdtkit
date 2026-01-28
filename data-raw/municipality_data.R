c0fdaa1353af8c71253d832fec8e921a1bbe71c6
Emma Skarstein <emma@skarstein.no>
2026-01-28 08:32:45 +0100

    fix encoding

--- data-raw/municipality_data.R
+++ data-raw/municipality_data.R
@@ -1,8 +1,20 @@
 
 # Save municipality names
-municipality_names <- read.table("data-raw/kommunenummer.csv", sep = ";", header = TRUE)
+municipality_names <- read.table(
+  "data-raw/kommunenummer.csv",
+  sep = ";",
+  header = TRUE,
+  encoding = "UTF-8",
+  fileEncoding = "UTF-8"
+)
 usethis::use_data(municipality_names, overwrite = TRUE, compress = "xz")
 
 # Save county names
-county_names <- read.table("data-raw/fylkesnummer.csv", sep = ";", header = TRUE)
+county_names <- read.table(
+  "data-raw/fylkesnummer.csv",
+  sep = ";",
+  header = TRUE,
+  encoding = "UTF-8",
+  fileEncoding = "UTF-8"
+)
 usethis::use_data(county_names, overwrite = TRUE, compress = "xz")
